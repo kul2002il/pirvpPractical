f8353e3ca9d8d76c5c777cccd2e1548e5a81d7f4
Ilia <kul.2002.il@gmail.com>
2021-01-14 11:16:39 +0700

    init

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Практическая работа</title>
+</head>
+<body>
+	<header>
+		<h2>Проверочная работа</h2>
+		<p>Выполнил Кулманаков Илья, группа 482.</p>
+	</header>
+	<main>
+		<article>
+			<h2>Задание 1</h2>
+			<p>
+				<?php
+					echo "create"
+				?>
+			</p>
+		</article>
+	</main>
+</body>
+</html>
