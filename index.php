23268a82bcd75580f1311a360d6f6788c9e1c355
Ilia <kul.2002.il@gmail.com>
2021-01-21 13:42:42 +0700

    Выполнено задание 2.

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
+<?php
+session_start();
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -18,27 +21,67 @@
 					{
 						try{
 							$filename = "https://www.php.net/manual/ru/function." . $func;
-							if (!file_exists($filename))
+							$otvet=@get_headers($filename);
+							$code = substr($otvet[0], 9, 3);
+							if ($code != '200')
 							{
-								throw new Exception();
+								throw new Exception($message = "Станица функции " . $func . " не найдена.");
 							}
 							$page = file_get_contents($filename);
 							$info = '';
 							if (!preg_match('#\\<div class="methodsynopsis dc-description"\\>.*?\\</div\\>#s', $page, $info))
 							{
-								throw new Exception();
+								throw new Exception($message = "Функция " . $func . " не найдена на станице.");
 							}
 							return $info[0];
 						}
 						catch (Exception $exc)
 						{
-							return "Функция " . $func . " не найдена.";
+							return $exc->getMessage();
 						}
 					}
 				}
 
-				echo "Описание функции sqrt: " . Finder::find('sqrtd')
+				echo "Описание функции sqrt: " . Finder::find('sqrt')
 				?>
+			</p>
+		</article>
+
+		<article>
+			<h2>Задание 2</h2>
+			<p>
+				<?php
+					class Session
+					{
+						public function set(string $key, $value)
+						{
+							$_SESSION[$key] = $value;
+						}
+
+						public function get(string $key)
+						{
+							return $_SESSION[$key];
+						}
+
+						public function existsKey(string $key)
+						{
+							return array_key_exists($key, $_SESSION);
+						}
+
+						public function listKeys()
+						{
+							return array_keys($_SESSION);
+						}
+					}
+
+					$ses = new Session();
+					$ses->set("key", "value");
+
+				?>
+				Значение key = <?= $ses->get('key') ?><br/>
+				Существование key = <?= $ses->existsKey('key') ?><br/>
+				Существование kei = <?= $ses->existsKey('kei') ?><br/>
+				Список ключей: <?php print_r($ses->listKeys()) ?>
 			</p>
 		</article>
 	</main>
