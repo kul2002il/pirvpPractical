c36bb91b3680f55bce6f0318d648edbccbba5b1a
Ilia <kul.2002.il@gmail.com>
2021-02-11 10:21:00 +0700

    Выполнено задание.

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
+<?php
+session_start()
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -13,7 +16,49 @@
 			<h2>Задание 1</h2>
 			<p>
 				<?php
-					echo "create"
+				trait Auth{
+					function __construct()
+					{
+						if(isset($this->email) && isset($this->password))
+						{
+							$this->auth = function (string $email, string $password)
+							{
+								$success = $this->email == $email && $this->password == $password;
+								if ($success)
+								{
+									$_SESSION["userAuth"] = $this->email;
+								}
+								return $success;
+							};
+
+							$this->is_auth = function ()
+							{
+								return $_SESSION["userAuth"] == $this->email;
+							};
+						}
+					}
+
+					public function __call($name, $arguments)
+					{
+						if(isset($this->$name))
+						{
+							$func = $this->$name;
+							return $func(...$arguments);
+						}
+						return null;
+					}
+				};
+
+				class User
+				{
+					use Auth;
+					public $email = "d";
+					public $password = "1";
+				}
+
+				$user = new User();
+				echo $user->auth("d", "1") . "<br/>";
+				echo $user->is_auth() . "<br/>";
 				?>
 			</p>
 		</article>
