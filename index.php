f40551460131c0f81636e2a0924c7d706e78239e
Ilia <kul.2002.il@gmail.com>
2021-02-27 09:51:37 +0700

    Сделан базовый класс.

--- index.php
+++ index.php
@@ -1,47 +1,17 @@
 <?php
 
-class Pager{
-	private $page = 0;
-	private $size = 10;
-	private $countPage = 10;
+require_once "pager.php";
 
-	public function __construct(int $page = 0, int $size = 10)
-	{
-		$this->start = $page;
-		$this->size = $size;
-	}
-
-	private final function getLink(){
-		$out = '<div>';
-		if($this->page != 0)
-		{
-			$out .= '<a href="?' . ($this->page - 1) . '">лево<a>';
-		}
-		for ($i = 0; $i < $this->countPage; $i++)
-		{
-			if($i == 0)
-			{
-				;
-			}
-			else
-			{
-				$out .= '<a href="?' . ($this->page + $i) . '"> ' . ($this->page + $i) . ' <a>';
-			}
-		}
-		if($this->page != $this->countPage)
-		{
-			$out .= '<a href="?' . ($this->page + 1) . '">право<a>';
-		}
-		$out .= '</div>';
-		return $out;
-	}
-
-	public final function getPage(){
-		$out = $this->getLink();
-		$out .= $this->getLink();
-		return $out;
-	}
+$page = 0;
+if(isset($_GET["page"]))
+{
+	$page = $_GET["page"];
+}
+$size = 3;
+if(isset($_GET["size"]))
+{
+	$page = $_GET["size"];
 }
 
-$pager = new Pager();
-echo $pager->getPage();
+$pager = new Pager($page, $size);
+$pager->show();
---
+++ pager.php
@@ -0,0 +1,80 @@
+<?php
+
+class Pager{
+	private $page = 0; // Номер открытой страницы.
+	private $size = 10; // Элементов на страницу.
+	private $countPage = 10; // Количество страниц.
+	private $data = []; // Массив данных на вывод.
+
+	public function __construct(int $page = 0, int $size = 3)
+	{
+		$this->page = $page;
+		$this->size = $size;
+		$this->data = $this->loadData();
+		$this->countPage = ceil(count($this->data) / $this->size);
+	}
+
+	public final function show()
+	{
+		echo $this->getPage();
+	}
+
+	public final function getPage(){
+		$links = $this->getLink();
+		$out = $links;
+		for ($i = $this->page * $this->size; $i < ($this->page + 1) * $this->size && $i < count($this->data); $i++)
+		{
+			$out .= $this->getArticle($i);
+		}
+		$out .= $links;
+		return $out;
+	}
+
+	private final function getLink(){
+		$out = '<div>';
+		if($this->page != 0)
+		{
+			$out .= '<a href="?page=' . ($this->page - 1) . '">лево</a>';
+		}
+		else
+		{
+			$out .= '<span">лево</span>';
+		}
+		$out .= '<span> ' . ($this->page + 1) . '/' . $this->countPage . ' </span>';
+		for ($i = 0; $i < $this->countPage; $i++)
+		{
+			if($i == $this->page)
+			{
+				$out .= '<span> ' . ($i+1) . ' </span>';
+			}
+			else
+			{
+				$out .= '<a href="?page=' . ($i) . '"> ' . ($i+1) . ' </a>';
+			}
+		}
+		if($this->page < $this->countPage - 1)
+		{
+			$out .= '<a href="?page=' . ($this->page + 1) . '">право</a>';
+		}
+		else
+		{
+			$out .= '<span">право</span>';
+		}
+		$out .= '</div>';
+		return $out;
+	}
+
+	public function getArticle($number)
+	{
+		$out = "<article>";
+		$out .= $this->data[$number];
+		$out .= "</article>";
+		return $out;
+	}
+
+	public function loadData()
+	{
+		return [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+	}
+}
+
