8ae29ff2aee9114cb0276ec6172beac0395ebc13
Ilia <kul.2002.il@gmail.com>
2021-03-25 11:01:35 +0700

    Шаблон v2.

--- template/Page.php
+++ template/Page.php
@@ -5,16 +5,38 @@ namespace template;
 class Page{
 	public function show()
 	{
-		echo
-"<!DOCTYPE html>
+		?>
+<!DOCTYPE html>
 <html>
 <head>
 	<title>Главная</title>
 	<meta charset='utf-8'>
+	<link rel="stylesheet" href="static/css/style.css">
 </head>
 <body>
-	<h1>Главная</h1>
+	<table>
+		<thead>
+			<tr>
+				<td class="header" colspan="3">
+					<h1>Главная</h1>
+				</td>
+			</tr>
+		</thead>
+		<tfoot>
+		<tr>
+			<td class="footer" colspan="3">
+				Сделано с безразличием. Дно
+			</td>
+		</tr>
+		</tfoot>
+		<tr>
+			<td class="left">Лево</td>
+			<td class="center">Центр</td>
+			<td class="right">Право</td>
+		</tr>
+	</table>
 </body>
-</html>";
+</html>
+<?php
 	}
 }
