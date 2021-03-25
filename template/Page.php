e4fa9da18b29a3b200b23d984b4e238cb89c2c90
Ilia <kul.2002.il@gmail.com>
2021-03-25 10:42:01 +0700

    Добавлен шаблон страницы.

---
+++ template/Page.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace template;
+
+class Page{
+	public function show()
+	{
+		echo
+"<!DOCTYPE html>
+<html>
+<head>
+	<title>Главная</title>
+	<meta charset='utf-8'>
+</head>
+<body>
+	<h1>Главная</h1>
+</body>
+</html>";
+	}
+}
